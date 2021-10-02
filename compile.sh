0556bc729253f191e623c6a997e8c7f52d156980
Evann Berthou <evann.berthou@gmail.com>
2021-10-02 13:52:17 +0200

    Premier commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+PROJECT=$1
+
+echo Compiling $PROJECT
+cp $PROJECT output/$PROJECT
+cd output
+auto-multiple-choice prepare --mode s --data data --filter plain --n-copies 1 $PROJECT > /dev/null 2> /dev/null
+rm $PROJECT
