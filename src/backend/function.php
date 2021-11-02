8c4f938ad147a67343709bf0d014c0806e07569f
BenjaminPellieux <benjaminpellieux5@gmail.com>
2021-11-02 23:01:29 +0100

    modif BD + ajour de fonction dans function.php

--- src/backend/function.php
+++ src/backend/function.php
@@ -8,7 +8,7 @@ function getqcms($id){
     $qcms=Array();
     $cnx=mysqli_connect($host,$login,$pwd) or die("erreur de connexion");
     mysqli_select_db($cnx,"projet");
-    $re=" select * from qcm where idprof in (select id from users where identifiant=$id)² order by matiere;"
+    $re=" select * from qcm where idprof in (select id from users where identifiant=$id) order by matiere;"
     $result=mysqli_query($cnx,$re);
     while ($qcm=mysqli_fetch_assoc($result)){
         $qcms[]=$qcm;       
@@ -31,7 +31,28 @@ function getusers(){
 }
 
 
+function getqcmquestions($id){
+    $allquestions=Array();
+    $cnx=mysqli_connect($host,$login,$pwd) or die("erreur de connexion");
+    mysqli_select_db($cnx,"projet");
+    $re="select * from question where idqcm in (select id from qcm where idqcm=$id);"
+    $result=mysqli_query($cnx,$re);
+    while ($question=mysqli_fetch_assoc($result)){
+        $allquestions[]=getqcmreponses($question);      
+    }
+    return $allquestions;
+}
 
-
+function getqcmreponses($question){
+    $allreponses=Array();
+    $cnx=mysqli_connect($host,$login,$pwd) or die("erreur de connexion");
+    mysqli_select_db($cnx,"projet");
+    $re="select * from reponse where idquestion  in (select id from question where id=$question[0]);"; 
+    $result=mysqli_query($cnx,$re);
+    while ($reponse=mysqli_fetch_assoc($result)){
+        $allreponses[]=$reponse;       
+    }
+    return $allreponses;
+}
 
 ?>
