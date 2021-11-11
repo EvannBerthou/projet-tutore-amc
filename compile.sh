981471815381f3475ccdfb623ef11b819dffffae
Evann Berthou <evann.berthou@gmail.com>
2021-11-11 13:45:29 +0100

    Réorganisation du projet: Passage à Symfony
    Après discussion entre les membres de l'équipe, nous avons décidé
    d'utiliser le framework Symfony pour le projet. Ce commit vise à mettre
    en place la nouvelle structure du projet avec un projet Symfone par
    défaut.

--- compile.sh
+++
@@ -1,8 +0,0 @@
-PREFIX=".."
-PROJECT=$1
-
-echo Compiling $PROJECT
-cp $PREFIX/questions/$PROJECT $PREFIX/output/$PROJECT
-cd $PREFIX/output
-auto-multiple-choice prepare --mode s --data data --filter plain --n-copies 1 $PROJECT > /dev/null 2> /dev/null
-rm $PROJECT
--- src/backend/param.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$host="localhost";
-$login="root";
-$pwd="";
-
-?>
