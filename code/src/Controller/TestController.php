8e6f7bd4c978123807bec7e3195762f0233a7820
Evann Berthou <evann.berthou@gmail.com>
2021-11-13 10:18:36 +0100

    Séparation des fichiers css par page

--- code/src/Controller/TestController.php
+++ code/src/Controller/TestController.php
@@ -6,19 +6,16 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  
 class TestController extends AbstractController{   
-    private $name="Alicia";
+    private $name = "Alicia";
 
-    #[Route('/main')]
-    public function page_utilisateur(): Response
-    {
-        $number = random_int(0, 100);
-        return $this -> render("page_utilisateur.html.twig",['user_name'=>$this->name]);
-}
+    #[Route('/main', name: "app_page_utilisateur")]
+    public function page_utilisateur(): Response {
+        return $this->render("page_utilisateur.html.twig", ['user_name' => $this->name]);
+    }
     
-    #[Route("/connexion", name: "connexion" )]
-    public function connexion(): Response
-    {
-        return $this -> render("connexion.html.twig");
+    #[Route("/connexion", name: "app_connexion" )]
+    public function connexion(): Response {
+        return $this->render("connexion.html.twig");
     }  
 
 
