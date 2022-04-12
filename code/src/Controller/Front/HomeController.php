56f0a5a01ba6b26da2078b424f03ac712b308c68
Evann Berthou <evann.berthou@gmail.com>
2022-04-12 20:23:27 +0200

    Test des vars d'env

--- code/src/Controller/Front/HomeController.php
+++ code/src/Controller/Front/HomeController.php
@@ -6,9 +6,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpKernel\KernelInterface;
+
 class HomeController extends AbstractController {
     #[Route("/accueil", name: "app_home")]
     public function home(): Response {
         return $this->render('home.html.twig');
+    }
+
+    #[Route("/env", name: "app_env")]
+    public function env(KernelInterface $kernel): Response {
+        return new Response($kernel->getEnvironment());
     }
 }
